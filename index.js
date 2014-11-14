1ab74a0957c3bfea9c1d811270fe034dbe2ba668
Simon Ertel <simon.ertel@twenga.com>
2014-11-14 14:21:05 +0100

    test

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-alert("gogol");
+module.exports = function set(params){
+    var canvasTarget = params.canvasTarget;
+    var colorBg = params.bgColor;
+    var colorFg = params.fgColor;
+    console.log(canvasTarget);
+
+    node = document.querySelector("canvas");
+    console.log(node);
+};
+
