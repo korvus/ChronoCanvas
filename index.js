02ecbbe4ae7b083d4599e9a8f4f9beb348e4ba42
Simon Ertel <simon.ertel@twenga.com>
2014-11-14 17:54:39 +0100

    inte fix chronoCanvas

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 module.exports = function set(params){
 
     var canvasTarget = params.canvasTarget;
-    var color = [];
+    var color = [], start = [], end = [];
     color[0] = "tomato";
-    color[1] = "ivory";
+    color[1] = "#000";
+    start[0] = Math.PI*1.5;
+    start[1] = Math.PI*1.5;
+    end[0] = (Math.PI*0.5)*-1;
+    end[1] = Math.PI*1.6;
 
     if(params.color1){color[0] = params.color1;}
     if(params.color2){color[1] = params.color2;}
@@ -13,13 +17,14 @@ module.exports = function set(params){
         centerX = elt.width/2;
         centerY = elt.height/2;
         radius  = centerX;
-        ctx.beginPath();
         for(var a=0;a<2;a++){
-            ctx.arc(centerX, centerY, radius, 0, Math.PI*2);
+            ctx.beginPath();
+            ctx.arc(centerX, centerY, radius, start[a], end[a]);
+            ctx.lineTo(radius,radius);
             ctx.fillStyle = color[a];
+            ctx.fill();
+            ctx.closePath();
         }
-        ctx.fill();
-        ctx.closePath();
 
     }
 
