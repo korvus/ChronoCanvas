680771b209d34f98683784121a08c542c104f11e
Simon Ertel <ertel.simon@gmail.com>
2014-11-15 02:53:29 +0100

    animation dev

--- index.js
+++ index.js
@@ -2,37 +2,51 @@ module.exports = function set(params){
 
     var canvasTarget = params.canvasTarget;
     var color = [], start = [], end = [];
+    var when = 0;
     color[0] = "tomato";
     color[1] = "#000";
-    start[0] = Math.PI*1.5;
-    start[1] = Math.PI*1.5;
-    end[0] = (Math.PI*0.5)*-1;
-    end[1] = Math.PI*1.6;
+    start[0] = 0;
+    start[1] = 0;
+    end[0] = 1;
+    end[1] = 0.25;
+    startC = Math.PI / 2;
+    endC = Math.PI * 2;
 
     if(params.color1){color[0] = params.color1;}
     if(params.color2){color[1] = params.color2;}
     
-    function drawCircle(elt, i){
-        var ctx = elt.getContext('2d');
+    function rate(rate){
+        return ((endC) * rate) - startC;
+    }
+
+    this.drawCircle = function(elt){
+        requestAnimationFrame(drawCircle);        
         centerX = elt.width/2;
         centerY = elt.height/2;
         radius  = centerX;
         for(var a=0;a<2;a++){
+            when = end[a]+when;
             ctx.beginPath();
-            ctx.arc(centerX, centerY, radius, start[a], end[a]);
+            ctx.arc(centerX, centerY, radius, -(startC), rate(end[a]), false);
             ctx.lineTo(radius,radius);
             ctx.fillStyle = color[a];
             ctx.fill();
             ctx.closePath();
         }
+        when = when+(1/100);
+        startTimer(elt);
+        clear(timer);
+    }
 
+    function startTimer(elt){
+        console.log("Ce qui est passé en param :"+elt);
+        timer = setTimeout(function(){this.drawCircle(elt);}, 20000);
     }
 
     window.onload = function(){
-
         var elt = document.querySelectorAll(canvasTarget);
         if(elt.length>0){
-            Array.prototype.forEach.call(elt,drawCircle);
+            Array.prototype.forEach.call(elt,startTimer);
         }
     }
 
